ff5cf52679ab18d8073333701ceddb64342bf06c
poppywydy <adeliasekar7@gmail.com>
2025-03-07 12:57:43 +0700

    edit fileReader.js

--- utils/fileReader.js
+++ utils/fileReader.js
@@ -2,20 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const readJSONFile = (relativeFilePath) => {
-  // Gunakan process.cwd() untuk memastikan path relatif ke root deployment
-  const filePath = path.join(process.cwd(), relativeFilePath);
-  console.log('Membaca file dari:', filePath, 'Ada?', fs.existsSync(filePath));
+  const filePath = path.join(__dirname, '..', relativeFilePath);
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
-        console.error('Error membaca file:', err);
         return reject(err);
       }
       try {
         const jsonData = JSON.parse(data);
         resolve(jsonData);
       } catch (parseError) {
-        console.error('Error parse JSON:', parseError);
         reject(parseError);
       }
     });
